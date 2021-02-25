03b81d77f7f4d6297bcf3008f9b2668ef86b3bcc
HarshJadhav77 <77331772+HarshJadhav77@users.noreply.github.com>
2021-02-25 15:17:00 +0530

    Add files via upload

---
+++ MyOwnLibrary.js
@@ -0,0 +1,25 @@
+function Touch(object1,object2)
+{
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2
+    && object1.y - object2.y < object2.height/2 + object1.height/2
+    && object2.y - object1.y < object2.height/2 + object1.height/2)
+    {
+       return true;
+    }
+      else 
+      { 
+        return false;
+      }
+}
+
+function Collide(object1,object2)
+{
+  if(object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2
+    && object1.y - object2.y < object2.height/2 + object1.height/2
+    && object2.y - object1.y < object2.height/2 + object1.height/2)
+    {
+      object1.setVelocity(0,0);
+    }
+}
